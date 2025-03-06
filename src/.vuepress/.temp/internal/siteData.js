05913130b85b16f0627472427ec434e64c4c6b7a
Jason Statham <xpt_notes@163.com>
2025-03-06 15:10:08 +0800

    init

--- src/.vuepress/.temp/internal/siteData.js
+++
@@ -1,1 +0,0 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"文档演示\",\"description\":\"vuepress-theme-hope 的文档演示\",\"head\":[],\"locales\":{}}")
--- src/.vuepress/.temp/markdown-stylize/config.js
+++
@@ -1,3 +0,0 @@
-import "/Users/xupengtao/Documents/note-space/notes-base/node_modules/.pnpm/@mdit+plugin-spoiler@0.16.0_markdown-it@14.1.0/node_modules/@mdit/plugin-spoiler/spoiler.css"
-
-
